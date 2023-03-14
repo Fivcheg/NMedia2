f14001b4bf079ec8fef3b42373e9c5c38467b390
Vitalii Novgorodov <vital00011@rambler.ru>
2023-03-14 18:34:04 +0300

    Edit errors

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -23,7 +23,7 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
     private val _data = MutableLiveData(FeedModel())
     val data: LiveData<FeedModel>
         get() = _data
-    val edited = MutableLiveData(empty)
+    private val edited = MutableLiveData(empty)
     private val _postCreated = SingleLiveEvent<Unit>()
     val postCreated: LiveData<Unit>
         get() = _postCreated
@@ -68,13 +68,17 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
         edited.value = edited.value?.copy(content = text)
     }
 
-    fun likeById() {
-        _data.value = FeedModel(loading = true)
+    fun likeById(id: Long, likedByMe: Boolean) {
+        repository.likeById(id, likedByMe, object : PostRepository.PostsCallback<Post> {
 
-        repository.getAll(object : PostRepository.PostsCallback<List<Post>> {
-
-            override fun onSuccess(data: List<Post>) {
-                _data.value = FeedModel(posts = _data.value?.posts.orEmpty())
+            override fun onSuccess(data: Post) {
+                _data.postValue(
+                    FeedModel(
+                        posts = _data.value?.posts.orEmpty()
+                            .map {
+                                if (it.id == id) data else it
+                            })
+                )
             }
 
             override fun onError(e: Exception) {
